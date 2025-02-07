f170866cbfe14dbebe5bf938876a787563e30578
Andrei Orlov <aaorlov1@gmail.com>
2025-02-07 14:47:34 +0300

    Udpate events

--- src/types/v1050.ts
+++ src/types/v1050.ts
@@ -3,3 +3,9 @@ import {sts, Result, Option, Bytes, BitSequence} from './support'
 export const Balance = sts.bigint()
 
 export const AccountId = sts.bytes()
+
+export const Hash = sts.bytes()
+
+export const ByteString = sts.bytes()
+
+export const CalculationResult = sts.bigint()
--- src/types/v9130.ts
+++ src/types/v9130.ts
@@ -1,3 +1,9 @@
 import {sts, Result, Option, Bytes, BitSequence} from './support'
 
 export const AccountId32 = sts.bytes()
+
+export const Hash = sts.bytes()
+
+export const ByteString = sts.bytes()
+
+export const CalculationResult = sts.bigint()
