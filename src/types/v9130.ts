75226b5b8d51e9d9bce2278635eb2898ea973f77
Andrei Orlov <aaorlov1@gmail.com>
2025-02-07 15:32:53 +0300

    Remove extra types

--- src/types/v9130.ts
+++
@@ -1,9 +0,0 @@
-import {sts, Result, Option, Bytes, BitSequence} from './support'
-
-export const AccountId32 = sts.bytes()
-
-export const Hash = sts.bytes()
-
-export const ByteString = sts.bytes()
-
-export const CalculationResult = sts.bigint()
