86ccbe7570a23a1327fb4f446eec758e12d9f023
Alisher A. Khassanov <a.khssnv@gmail.com>
2026-01-29 19:36:52 +0500

    Add calls to processor

--- src/processor.ts
+++ src/processor.ts
@@ -23,6 +23,10 @@ export const processor = new SubstrateBatchProcessor()
         // More RPC connection options at https://docs.subsquid.io/substrate-indexing/setup/general/#set-data-source
         // rateLimit: 10 // requests per second, default is no limit
     })
+    .addCall({
+        name: ["*"],
+        extrinsic: true
+    })
     .addEvent({
         name: [
             events.balances.transfer.name,
@@ -30,6 +34,15 @@ export const processor = new SubstrateBatchProcessor()
         extrinsic: true
     })
     .setFields({
+        call: {
+            extrinsicIndex: true,
+            address: true,
+            name: true,
+            args: true,
+            origin: true,
+            error: true,
+            success: true
+        },
         event: {
             args: true
         },
